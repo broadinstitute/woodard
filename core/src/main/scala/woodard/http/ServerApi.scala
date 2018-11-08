55c37cd1a63723de7574578c43bdba6b71bffcda
Oliver Ruebenacker <oliverr@broadinstitute.org>
2018-11-08 18:45:31 -0500

    Submitting a workflow, part 1.

--- core/src/main/scala/woodard/http/ServerApi.scala
+++ core/src/main/scala/woodard/http/ServerApi.scala
@@ -2,9 +2,10 @@ package woodard.http
 
 import cats.effect.IO
 import org.http4s.MediaType.`application/json`
-import org.http4s.Method.GET
+import org.http4s.Method.{GET, POST}
 import org.http4s.client.dsl.io._
 import org.http4s.headers.Accept
+import org.http4s.multipart.Multipart
 import org.http4s.{Request, Uri}
 
 case class ServerApi(uri: Uri, version: String = "v1") {
@@ -15,6 +16,13 @@ case class ServerApi(uri: Uri, version: String = "v1") {
   )
 
   def getWorkflowApi(id: String): ServerWorkflowApi = ServerWorkflowApi(this, id)
+
+  def submit: IO[Request[IO]] = POST(
+    uri / "api" / "workflows" / version,
+    Multipart(Vector(
+
+    ))
+  )
 
 }
 
--- cromiam/src/test/scala/woodard/http/auth/CromiamSubmitTest.scala
+++ cromiam/src/test/scala/woodard/http/auth/CromiamSubmitTest.scala
@@ -14,17 +14,25 @@ class CromiamSubmitTest extends FunSuite {
     baseCredentials.withScopes(Scope.all)
   }
 
-  test("Try to submit to CromIam server") {
+  private val credentials = getCredentials
+
+  test("Metadata request to CromIam server") {
     val client = Http1Client[IO]().unsafeRunSync()
     val workflowId = "9d9d51b5-a2d5-45e2-a2f5-40287fad1eb3"
     val requestIO = ServerApi.caasProd.getWorkflowApi(workflowId).getMetadata
     println(requestIO.map(_.uri.renderString).unsafeRunSync())
-    val credentials = getCredentials
     val stringIO = client.expect[String](requestIO.map(credentials.addToRequestOpt(_).get))
     val string = stringIO.unsafeRunSync()
+    client.shutdownNow()
     println("yo!")
     println(string)
     println("wassup?")
+  }
+
+  test("Workflow submission to CromIam server") {
+    val client = Http1Client[IO]().unsafeRunSync()
+    // TODO
+    client.shutdownNow()
   }
 
 }
