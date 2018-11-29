93b7f62538389f488593b4a1862b69dee4a9c3cf
Oliver Ruebenacker <oliverr@broadinstitute.org>
2018-11-29 15:23:21 -0500

    Adding WoodardShell, part 2. Version upgrade 0.0.1 to 0.1.0.

--- build.sbt
+++ build.sbt
@@ -1,5 +1,5 @@
 
-
+val woodardV = "0.1.0"
 val scalaV = "2.12.7"
 val scalatestV = "3.0.5"
 val http4sV = "0.18.21"
@@ -9,7 +9,7 @@ val logbackClassicV = "1.2.3"
 val circeV = "0.10.1"
 
 lazy val commonSettings = Seq(
-  version := "0.0.1",
+  version := woodardV,
   organization := "org.broadinstitute",
   scalaVersion := scalaV,
   resolvers ++= Seq(
--- cromiam/src/main/scala/woodard/woosh/WoodardShell.scala
+++ cromiam/src/main/scala/woodard/woosh/WoodardShell.scala
@@ -1,8 +1,11 @@
 package woodard.woosh
 
+import java.util.Date
+
 import better.files.File
 import woodard.http.auth.GoogleCredentialsBox
 import woodard.http.{HttpRequests, ServerApi}
+import woodard.model.{WorkflowMetadataRequest, WorkflowMetadataResponse, WorkflowStatusRequest, WorkflowStatusResponse, WorkflowSubmitRequest, WorkflowSubmitResponse}
 
 class WoodardShell(var serverApi: ServerApi) {
 
@@ -10,6 +13,23 @@ class WoodardShell(var serverApi: ServerApi) {
   var inputFileOpt: Option[File] = None
   var optionsFileOpt: Option[File] = None
   var collectionNameOpt: Option[String] = None
+
+  private def timeString: String = new Date(System.currentTimeMillis()).toString
+
+  private def log(message: String): Unit = println(timeString + " : " + message)
+
+  private def error(message: String): Unit = log("ERROR: " + message)
+
+  private def checkFileExists(file: File, isOptional: Boolean = false): Unit = {
+    if (!file.exists) {
+      val message = s"File $file does not exist."
+      if (isOptional) {
+        log(message)
+      } else {
+        error(message)
+      }
+    }
+  }
 
   def httpRequests_=(httpRequests: HttpRequests): Unit = {
     serverApi = serverApi.withHttpRequests(httpRequests)
@@ -20,28 +40,22 @@ class WoodardShell(var serverApi: ServerApi) {
       val credentials = GoogleCredentialsBox.forServiceAccount(authFile).withAllScopes
       serverApi = serverApi.map(credentials.addToRequest)
     } else {
-      println(s"ERROR: File $authFile does not exist.")
-    }
-  }
-
-  private def checkExists(file: File): Unit = {
-    if (!file.exists) {
-      println(s"WARNING: File $file does not exist.")
+      error(s"File $authFile does not exist.")
     }
   }
 
   def source_=(file: File): Unit = {
-    checkExists(file)
+    checkFileExists(file)
     sourceFileOpt = Some(file)
   }
 
   def inputs_=(file: File): Unit = {
-    checkExists(file)
+    checkFileExists(file)
     inputFileOpt = Some(file)
   }
 
   def options_=(file: File): Unit = {
-    checkExists(file)
+    checkFileExists(file)
     optionsFileOpt = Some(file)
   }
 
@@ -49,13 +63,51 @@ class WoodardShell(var serverApi: ServerApi) {
     collectionNameOpt = Some(name)
   }
 
-  def submit(): Unit = {
-    ??? // TODO
+  var lastWorkflowIdOpt: Option[String] = None
+
+  def submit(): Either[String, WorkflowSubmitResponse] = {
+    sourceFileOpt.foreach(checkFileExists(_))
+    inputFileOpt.foreach(checkFileExists(_))
+    optionsFileOpt.foreach(checkFileExists(_, isOptional = true))
+    sourceFileOpt match {
+      case Some(sourceFile) =>
+        val request = WorkflowSubmitRequest(sourceFile, inputFileOpt, optionsFileOpt, collectionNameOpt)
+        log(s"Submitting $request")
+        val response = serverApi.workflowSubmit(request)
+        lastWorkflowIdOpt = Some(response.id)
+        Right(response)
+      case None =>
+        Left("No workflow source file specified.")
+    }
+  }
+
+  def getWorkflowId(idTemplate: String): String = {
+    if (idTemplate.isEmpty && lastWorkflowIdOpt.nonEmpty) {
+      lastWorkflowIdOpt.get
+    } else {
+      idTemplate
+    }
+  }
+
+  def status(idTemplate: String): Either[String, WorkflowStatusResponse] = {
+    val id = getWorkflowId(idTemplate)
+    val request = WorkflowStatusRequest(id)
+    val response = serverApi.workflowStatus(request)
+    Right(response)
+  }
+
+  def metadata(idTemplate: String): Either[String, WorkflowMetadataResponse] = {
+    val id = getWorkflowId(idTemplate)
+    val request = WorkflowMetadataRequest(id)
+    val response = serverApi.workflowMetadata(request)
+    Right(response)
   }
 
 }
 
 object WoodardShell {
+
+  val caasProd: WoodardShell = new WoodardShell(ServerApi(HttpRequests.caasProd))
 
 }
 
